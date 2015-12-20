696a6407714beec3375f4f637dcf6d12cb33d93b
Lucytheanimefan <spothorse9.lucy@gmail.com>
2015-12-20 19:37:59 +0800

    Added Site files

---
+++ app.js
@@ -0,0 +1,29 @@
+var main = function() {
+/*$('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+ 
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};*/
+
+var button=document.querySelector('myButton');
+button.addEventListener('click',function(){
+	window.location="http://www.animemaru.com/tokyo-ghoul-season-3-confirmed/";
+});
+
+$(document).ready(main)
